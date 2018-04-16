9e86a181c372d221796c05e8338f21a186a81c12
delatetei <ambulamce@gmail.com>
2018-04-17 07:14:38 +0900

    viのモードライン追加、インデント数修正

--- effective-highlighter.php
+++ effective-highlighter.php
@@ -1,10 +1,14 @@
 <?php
-   /*
-   Plugin Name: Effective Highlighter
-   Plugin URI: https://delatetei.net/effective_highlighter/
-   Description: A plugin to add effective text highlight option to the WordPress editor.
-   Version: 1.0
-   Author: delatetei
-   Author URI: https://delatetei.net/
-   License: GPL2
-   */
+  /*
+  Plugin Name: Effective Highlighter
+  Plugin URI: https://delatetei.net/effective_highlighter/
+  Description: A plugin to add effective text highlight option to the WordPress editor.
+  Version: 1.0
+  Author: delatetei
+  Author URI: https://delatetei.net/
+  License: GPL2
+  */
+    
+/* -*- coding: utf-8-unix -*- */
+// vim: et fenc=utf-8 ff=unix sw=2 ts=2 sts=2
+
