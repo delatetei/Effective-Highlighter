5cd67c559a8d8cfce9c3745e712af6bc021155a7
delatetei <ambulamce@gmail.com>
2018-04-17 07:12:26 +0900

    プラグイン情報を追加

---
+++ effective-highlighter.php
@@ -0,0 +1,10 @@
+<?php
+   /*
+   Plugin Name: Effective Highlighter
+   Plugin URI: https://delatetei.net/effective_highlighter/
+   Description: A plugin to add effective text highlight option to the WordPress editor.
+   Version: 1.0
+   Author: delatetei
+   Author URI: https://delatetei.net/
+   License: GPL2
+   */
