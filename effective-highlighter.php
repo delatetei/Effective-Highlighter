e112c13f7d96eec26de4f773d3628227f5b20754
delatetei <ambulamce@gmail.com>
2018-04-17 07:55:59 +0900

    エディタのスタイルプルダウンに専用の項目を追加

--- effective-highlighter.php
+++ effective-highlighter.php
@@ -8,6 +8,42 @@
   Author URI: https://delatetei.net/
   License: GPL2
   */
+
+  if (!function_exists('initialize_tinymce_styles')):
+  function initialize_tinymce_styles($init_array) {
+    $style_formats = array(
+      array(
+        'title' => 'Effective Highlighter',
+        'items' => array(
+          array(
+            'title' => 'pink',
+            'inline' => 'strong',
+            'classes' => 'effective-highlighter-pink'
+          ),
+          array(
+            'title' => 'blue',
+            'inline' => 'strong',
+            'classes' => 'effective-highlighter-blue'
+          ),
+        )
+      )
+    );
+    $init_array['style_formats'] = json_encode($style_formats);
+    return $init_array;
+  }
+  endif;
+  add_filter('tiny_mce_before_init', 'initialize_tinymce_styles', 10000);
+  
+  if ( !function_exists( 'add_styles_to_tinymce_buttons' ) ):
+  function add_styles_to_tinymce_buttons($buttons) {
+    $temp = array_shift($buttons);
+    array_unshift($buttons, 'styleselect');
+    array_unshift($buttons, $temp);
+  
+    return $buttons;
+  }
+  endif;
+  add_filter('mce_buttons_2','add_styles_to_tinymce_buttons');
     
 /* -*- coding: utf-8-unix -*- */
 // vim: et fenc=utf-8 ff=unix sw=2 ts=2 sts=2
